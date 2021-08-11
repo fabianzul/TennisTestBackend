54e5f688e7998311fd5515a28004d1a69b458c9d
Carlos <czuluaga@omnixcorp.com>
2021-08-11 10:33:29 -0500

    Cross Origin restore

--- src/main/java/com/tennis/revision/RevisionApplication.java
+++ src/main/java/com/tennis/revision/RevisionApplication.java
@@ -17,28 +17,4 @@ public class RevisionApplication {
 		SpringApplication.run(RevisionApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/clients").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
-
-	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://localhost:4200");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
-	}
-
 }
